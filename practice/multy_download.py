d1130c7867206fce370b77820ccbbb5e90c798ff
vihaan barupal <159883311+Dineshkumarbarupal@users.noreply.github.com>
2024-11-29 13:30:41 +0530

    new changes

--- practice/multy_download.py
+++ practice/multy_download.py
@@ -1,5 +1,6 @@
-import multiprocessing
+# import multiprocessing
 import requests
+import concurrent.futures
 
 def downloadFile(Url,name):
     print(f"start downloding {name}.")
@@ -7,18 +8,25 @@ def downloadFile(Url,name):
     open(f"practice/files/file{name}.jpg", "wb").write(responss.content)
     print(f"finished downloding {name}.")
 if __name__== '__main__':
-    proc = [] 
+    # proc = [] 
     Url = "https://picsum.photos/2000/3000"
-    for i in range(5):
-        # downloadFile(Url , i)
-        p = multiprocessing.Process(target = downloadFile , args = [Url, i])
-        p.start()
-        proc.append(p)
+    # for i in range(5):
+    #     # downloadFile(Url , i)
+    #     p = multiprocessing.Process(target = downloadFile , args = [Url, i])
+    #     p.start()
+    #     proc.append(p)
 
-    for p in proc:
-        p.join()   
+    # for p in proc:
+    #     p.join()   
+    
 
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        l1 = [Url for i in range(60)]
+        l2 = [i for i in range(60)]
 
+        result = executor.map(downloadFile,l1,l2)
+        for r in result:
+            print(r)        
 
 
 
